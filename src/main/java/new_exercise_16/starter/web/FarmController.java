e0da54088da39230f207a0d0ec9b4ff1b5e2fe66
Chris <cr1s@hotmail.it>
2024-07-19 15:30:03 +0200

    fix

--- src/main/java/new_exercise_16/starter/web/FarmController.java
+++ src/main/java/new_exercise_16/starter/web/FarmController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -68,7 +69,7 @@ public class FarmController {
         return ResponseEntity.ok(f);
     }
 
-    @GetMapping("delete/{id}")
+    @DeleteMapping("delete/{id}")
     public ResponseEntity<String> deletefarm(@PathVariable int id){
         Optional<Farm> optF = service.getFarmById(id);
         if(optF.isEmpty()){
